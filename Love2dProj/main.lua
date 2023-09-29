310dbb48682db72bd5deef9b6a69f847d8e0bd49
CaroPohn <carolinapohn@gmail.com>
2023-09-29 12:44:29 -0300

    Add main file and example sprite

---
+++ Love2dProj/main.lua
@@ -0,0 +1,32 @@
+function love.load()
+    x = 100
+    y = 300
+
+    sheep = love.graphics.newImage("sheep.png")
+end
+
+function love.update(dt)
+    input(dt)
+end
+
+function love.draw()
+    love.graphics.draw(sheep, x, y, 2, 2, 2)
+end
+
+function input(dt)
+    if love.keyboard.isDown("right") then
+        x = x + 100 * dt
+    end
+    
+    if love.keyboard.isDown("left") then
+        x = x - 100 * dt
+    end
+
+    if love.keyboard.isDown("up") then
+        y = y - 100 * dt
+    end
+    
+    if love.keyboard.isDown("down") then
+        y = y + 100 * dt
+    end
+end
